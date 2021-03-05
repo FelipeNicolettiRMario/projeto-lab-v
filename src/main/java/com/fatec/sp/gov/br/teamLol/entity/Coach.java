89de25819e8dfcc6b2300158bbffca05e42b13ab
FelipeNicolettiRMario <felipenicolettirmario@gmail.com>
2021-03-05 23:27:50 +0000

    Mapeamento Coach

---
+++ src/main/java/com/fatec/sp/gov/br/teamLol/entity/Coach.java
@@ -0,0 +1,39 @@
+package com.fatec.sp.gov.br.teamLol.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "cch_coach")
+public class Coach {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "cch_id")
+    private Long id;
+
+    @Column(name = "cch_nome")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
