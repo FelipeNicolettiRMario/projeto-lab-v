f3cfbe7929959d6ee51b1227a040d8dff514fb19
FelipeNicolettiRMario <felipenicolettirmario@gmail.com>
2021-03-06 00:16:19 +0000

    Repositorios faltantes

---
+++ src/main/java/com/fatec/sp/gov/br/teamLol/repository/CoachRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.sp.gov.br.teamLol.repository;
+
+import com.fatec.sp.gov.br.teamLol.entity.Coach;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoachRepository extends JpaRepository<Coach,Long>{
+    
+}
---
+++ src/main/java/com/fatec/sp/gov/br/teamLol/repository/TimeRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.sp.gov.br.teamLol.repository;
+
+import com.fatec.sp.gov.br.teamLol.entity.Time;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeRepository extends JpaRepository<Time,Long>{
+    
+}
