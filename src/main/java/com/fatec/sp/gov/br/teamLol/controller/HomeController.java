a5602f461a56bf4f1fff7dd84a8a6e98f68b53cb
FelipeNicolettiRMario <felipenicolettirmario@gmail.com>
2021-04-15 23:32:16 +0000

    Primeiro controller

---
+++ src/main/java/com/fatec/sp/gov/br/teamLol/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.fatec.sp.gov.br.teamLol.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HomeController {
+    
+    @GetMapping
+    public String welcome(){
+        return "Hello World!";
+    }
+}
